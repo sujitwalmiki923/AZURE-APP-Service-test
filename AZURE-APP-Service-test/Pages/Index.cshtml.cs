3b5e8cee7fb42dceb3f9a926035708df4abcd514
Admin <Admin@DESKTOP-9UNBO3Q>
2026-06-26 00:22:38 +0530

    Add project files.

---
+++ AZURE-APP-Service-test/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AZURE_APP_Service_test.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
