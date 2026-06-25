00657b2bb599f5b730f93fb092177f5479b1d719
sujit walmiki <sujitwalmiki923@gmail.com>
2026-06-26 02:21:28 +0530

    Added Greeting Message in Model and UI Razor Change

--- AZURE-APP-Service-test/Pages/Index.cshtml.cs
+++ AZURE-APP-Service-test/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace AZURE_APP_Service_test.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IConfiguration _configuration;
+        public string? Greeting { get; set; }
+        public IndexModel(ILogger<IndexModel> logger , IConfiguration configuration)
         {
             _logger = logger;
+            this._configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            Greeting = _configuration["Greeting"];
         }
     }
 }
