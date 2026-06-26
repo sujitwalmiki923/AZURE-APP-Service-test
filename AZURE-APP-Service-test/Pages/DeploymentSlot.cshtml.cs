9cd000090bbaf3bb96affca31e2679fc168febda
sujit walmiki <sujitwalmiki923@gmail.com>
2026-06-26 19:12:56 +0530

    Added Staging Branch for Deployment Slot

---
+++ AZURE-APP-Service-test/Pages/DeploymentSlot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AZURE_APP_Service_test.Pages
+{
+    public class DeploymentSlotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
